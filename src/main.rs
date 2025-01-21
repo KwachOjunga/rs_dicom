2ce239b9589435b63cf10f7b5f4d95210fd7e7e7
KwachOjunga <ojungakwach@gmail.com>
2025-01-21 18:02:13 +0300

    start impl CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+//! This file illustrates various ways of manipulating  a dicom file via a command line interface.
+use clap::{Arg, Parser};
+use rs_dicom::show_number_of_images;
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about,
+    long_about = "cli to display information about dicom files"
+)]
+struct Args {
+    /// file to display
+    #[arg(long)]
+    file: String,
+
+    /// frame number to dump
+    #[arg(short, long)]
+    image_to_dump: u8,
+}
+
+//parse arguments and pass them to functions from lib.rs
+
+//
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
