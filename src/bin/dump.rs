eb265ea5ac7a0ec14fca3180ab538f799479890c
KwachOjunga <ojungakwach@gmail.com>
2025-01-23 13:43:14 +0300

    alter dir structure

--- src/bin/dump.rs
+++ src/bin/dump.rs
@@ -1,9 +1,7 @@
-use dicom::dump::dump_file;
-use dicom::object::open_file;
 use rs_dicom::display_metadata;
 
 fn main() {
-	//let file = open_file("/mnt/c/Users/user/Downloads/dicom_viewer_0002/0002.DCM").unwrap();
-	//dump_file(&file).unwrap();
-	display_metadata("/mnt/c/Users/user/Downloads/dicom_viewer_0002/0002.DCM".into());
+    //let file = open_file("/mnt/c/Users/user/Downloads/dicom_viewer_0002/0002.DCM").unwrap();
+    //dump_file(&file).unwrap();
+    display_metadata("/mnt/c/Users/user/Downloads/dicom_viewer_0002/0002.DCM".into());
 }
