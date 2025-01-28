8f588dd8e7106793374a235d170ddae73dd6acef
KwachOjunga <ojungakwach@gmail.com>
2025-01-28 11:19:31 +0300

    delete dummy bin files in rs_dicom

--- src/bin/dump.rs
+++
@@ -1,7 +0,0 @@
-use rs_dicom::display_metadata;
-
-fn main() {
-    //let file = open_file("/mnt/c/Users/user/Downloads/dicom_viewer_0002/0002.DCM").unwrap();
-    //dump_file(&file).unwrap();
-    display_metadata("/mnt/c/Users/user/Downloads/dicom_viewer_0002/0002.DCM".into());
-}
